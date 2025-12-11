8aefe10bcbf5591ce54f4560585af7858498e812
raselrzl <rasel6041@gmail.com>
2025-12-11 13:44:40 +0100

    just added admin company employee pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import LoginForm from "./components/LoginForm";
-import UserRegistrationForm from "./components/UserRegistrationForm";
+"use client"; // <<< add this at the top
 
+import LoginForm from "./components/LoginForm";
 
 export default function Home() {
   return (
