cf899a153d3e4c306e83124329c098cd2886bfe5
raselrzl <rasel6041@gmail.com>
2025-12-31 10:14:37 +0100

    login ui fixed

--- app/(company)/company/companycomponents/AuthStatusPopup.tsx
+++ app/(company)/company/companycomponents/AuthStatusPopup.tsx
@@ -29,38 +29,38 @@ export default function AuthStatusPopup({
 
   return (
     <div className="fixed inset-0 z-9999 bg-black/40 flex items-center justify-center">
-      <div className="bg-white w-full max-w-sm p-6 rounded shadow-xl text-center">
-        <h2 className="text-xl font-bold mb-2">{employeeName}</h2>
+      <div className="bg-white w-[250px] rounded shadow-xl text-center">
+        <h2 className="text-xl font-bold text-gray-200 bg-teal-500 pb-4">Hi, {employeeName} <br/><span className="text-xs">You are sucessfully logged in</span></h2>
 
         {status === "LOGGED_IN_WITH_SCHEDULE" && schedule && (
-          <>
+          <div className="p-6">
             <CheckCircle className="w-10 h-10 text-green-500 mx-auto mb-2" />
             <p className="font-semibold">You have a schedule today</p>
             <p className="text-lg font-bold mt-1">
               {formatTime(schedule.startTime)} –{" "}
               {formatTime(schedule.endTime)}
             </p>
-          </>
+          </div>
         )}
 
         {status === "LOGGED_IN_NO_SCHEDULE" && (
-          <>
+          <div className="p-6">
             <Info className="w-10 h-10 text-blue-500 mx-auto mb-2" />
             <p className="font-semibold">Logged in</p>
             <p className="text-sm text-gray-600">
               You don’t have a schedule today
             </p>
-          </>
+          </div>
         )}
 
         {status === "ALREADY_LOGGED_IN" && (
-          <>
+          <div className="p-6">
             <AlertTriangle className="w-10 h-10 text-yellow-500 mx-auto mb-2" />
             <p className="font-semibold">Already logged in</p>
             <p className="text-sm text-gray-600">
               If you want to log out, please use your logout button
             </p>
-          </>
+          </div>
         )}
 
         <button
