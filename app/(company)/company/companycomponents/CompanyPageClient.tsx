38fe45a2d7efd56f2becf71a1a6275c411690994
raselrzl <rasel6041@gmail.com>
2026-01-03 00:27:28 +0100

    srart page login and logout logic change

--- app/(company)/company/companycomponents/CompanyPageClient.tsx
+++ app/(company)/company/companycomponents/CompanyPageClient.tsx
@@ -7,14 +7,7 @@ import {
   ClipboardClock,
   ClockAlert,
   ClockCheck,
-  CornerRightUp,
-  CornerUpLeft,
-  LogIn,
-  LogOut,
-  TimerOff,
-  User2,
   UsersIcon,
-  UserSquare,
 } from "lucide-react";
 
 import { loginEmployeeWithPin, logoutEmployeeWithPin } from "../companyactions";
@@ -32,10 +25,16 @@ function safeTime(value?: string | Date | null, mounted?: boolean) {
   });
 }
 
+// Start of day is 5:00 AM
 function startOfToday() {
   const d = new Date();
-  d.setHours(0, 0, 0, 0);
+  d.setHours(5, 0, 0, 0);
   return d.getTime();
+}
+
+// End of day is 5:00 AM next day
+function endOfToday() {
+  return startOfToday() + 24 * 60 * 60 * 1000;
 }
 
 // ---------------- COMPONENT ----------------
@@ -51,7 +50,8 @@ export default function CompanyPageClient({ companyData }: any) {
   const [selectedEmployee, setSelectedEmployee] = useState<any>(null);
   const [personalNumber, setPersonalNumber] = useState("");
 
-  const todayTime = startOfToday();
+  const todayStart = startOfToday();
+  const todayEnd = endOfToday();
 
   useEffect(() => setMounted(true), []);
 
@@ -86,233 +86,129 @@ export default function CompanyPageClient({ companyData }: any) {
     <div>
       <div className="max-w-7xl mx-auto px-2 py-10 mt-12">
         <div className="overflow-x-auto">
-          <div className="flex justify-end items-end">
-           
+          <div className="bg-teal-100 flex justify-between items-center">
+            <h1 className="px-4 py-2 uppercase text-xl md:text-2xl flex items-center bg-teal-100 text-teal-800 font-bold">
+              <UsersIcon className="mr-2" />
+              today’s team
+            </h1>
             <Link
               href="/company/schedule"
-              className="border border-teal-200 px-2 py-1 text-xs md:text-sm bg-teal-200 hover:bg-teal-100"
+              className="border mx-2 px-2 py-1 text-xs md:text-sm hover:bg-teal-200"
             >
               Check Schedule →
             </Link>
           </div>
 
-          {/* <table className="min-w-full border-collapse text-left">
-            <tbody>
-              {company.employees.map((emp: any, idx: number) => {
-                const todayLogs =
-                  emp.timeLogs?.filter((log: any) => {
-                    if (!log.logDate) return false;
-                    return (
-                      new Date(log.logDate).setHours(0, 0, 0, 0) === todayTime
-                    );
-                  }) || [];
+          <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-2 mt-2">
+            {company.employees.map((emp: any) => {
+              const todayLogs =
+                emp.timeLogs?.filter((log: any) => {
+                  if (!log.loginTime) return false;
+                  const login = new Date(log.loginTime).getTime();
+                  const logout = log.logoutTime ? new Date(log.logoutTime).getTime() : null;
 
-                const isLoggedIn =
-                  todayLogs.length > 0 &&
-                  !todayLogs[todayLogs.length - 1]?.logoutTime;
+                  // Include logs that are relevant to today (5 AM to next 5 AM)
+                  return (
+                    (login >= todayStart && login < todayEnd) || 
+                    (login < todayStart && (!logout || logout >= todayStart))
+                  );
+                }) || [];
 
-                const todaysSchedule = emp.schedules?.length
-                  ? emp.schedules.map((s: any, i: number) => (
-                      <div key={i} className="flex items-center gap-1">
-                        <CalendarDays className="w-4 h-4 text-teal-800" />
-                        <span>
-                          {new Date(s.startTime).toLocaleTimeString("en-GB", {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}{" "}
-                          -{" "}
-                          {new Date(s.endTime).toLocaleTimeString("en-GB", {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </span>
+              const lastLog = todayLogs[todayLogs.length - 1];
+              const isLoggedIn = lastLog ? !lastLog.logoutTime : false;
+
+              const todaysSchedule = emp.schedules?.length
+                ? emp.schedules.map((s: any, i: number) => (
+                    <div key={i} className="flex items-center text-xs">
+                      <CalendarDays className="w-3 h-3 text-teal-800" />
+                      <span>
+                        {new Date(s.startTime).toLocaleTimeString("en-GB", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}{" "}
+                        -{" "}
+                        {new Date(s.endTime).toLocaleTimeString("en-GB", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </span>
+                    </div>
+                  ))
+                : "—";
+
+              return (
+                <div
+                  key={emp.id}
+                  role="button"
+                  tabIndex={0}
+                  className={`cursor-pointer flex p-2 relative rounded-xs ${
+                    isLoggedIn
+                      ? "bg-teal-600"
+                      : "bg-gray-400 border border-gray-300"
+                  }`}
+                >
+                  {/* LEFT SIDE */}
+                  <div className="flex flex-col gap-1">
+                    {/* NAME + IMAGE */}
+                    <div className="flex items-center gap-2">
+                      <img src="/avater.png" className="w-10 h-10 rounded-full" />
+                      <div className="font-bold uppercase text-lg text-gray-200">
+                        {emp.name}
                       </div>
-                    ))
-                  : "—";
+                    </div>
 
-                const rowBg = idx % 2 === 0 ? "bg-white" : "bg-teal-50";
-
-                return (
-                  <tr
-                    key={emp.id}
-                    className={`${rowBg} border border-teal-100`}
-                  >
-                    <td className="px-4 py-2 font-medium text-teal-900">
-                      {emp.name}
-                    </td>
-
-                    <td className="px-4 py-2 text-teal-800">
-                      <div className="flex flex-col gap-1 text-sm">
+                    {/* SCHEDULE + LOGS */}
+                    <div>
+                      <div className="flex flex-col text-sm">
                         {todaysSchedule}
 
-                        <div className="mt-2 text-xs flex flex-col gap-1">
-                          {todayLogs.length === 0 && (
-                            <div className="flex items-center gap-1 text-gray-500 text-xs">
-                              <ClipboardClock className="w-4 h-4" />
-                              --:--
-                            </div>
-                          )}
+                        {todayLogs.length === 0 && (
+                          <div className="flex items-center gap-1 text-gray-500 text-xs mt-1">
+                            <ClipboardClock className="w-3 h-3" />
+                            00:00
+                          </div>
+                        )}
 
-                          {todayLogs.map((log: any, i: number) => (
-                            <div key={i} className="flex gap-3">
-                              {log.loginTime && (
-                                <div className="flex items-center gap-1 text-green-700">
-                                  <ClipboardClock className="w-4 h-4" />
-                                  {safeTime(log.loginTime, mounted)}
-                                </div>
-                              )}
-                              {log.logoutTime && (
-                                <div className="flex items-center gap-1 text-amber-700">
-                                  <TimerOff className="w-4 h-4" />
-                                  {safeTime(log.logoutTime, mounted)}
-                                </div>
-                              )}
-                            </div>
-                          ))}
-                        </div>
+                        {todayLogs.map((log: any, i: number) => (
+                          <div key={i} className="flex gap-2 text-xs mt-1">
+                            {log.loginTime && (
+                              <div className="flex items-center text-xs">
+                                <ClipboardClock className="w-3 h-3" />
+                                {safeTime(log.loginTime, mounted)}
+                              </div>
+                            )}
+                            {log.logoutTime && (
+                              <div className="flex items-center gap-1">
+                                - {safeTime(log.logoutTime, mounted)}
+                              </div>
+                            )}
+                          </div>
+                        ))}
                       </div>
-                    </td>
-
-                    <td className="px-4 py-2">
-                      {isLoggedIn ? (
-                        <button
-                          onClick={() => openAuth(emp, "logout")}
-                          className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700"
-                        >
-                          Logout
-                        </button>
-                      ) : (
-                        <button
-                          onClick={() => openAuth(emp, "login")}
-                          className="px-3 py-1 text-teal-700/60 hover:underline text-xs md:text-sm"
-                        >
-                          Not logged in
-                        </button>
-                      )}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table> */}
-
-          <div className="">
-            <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-2 mt-2">
-              {company.employees.map((emp: any, idx: number) => {
-                const todayLogs =
-                  emp.timeLogs?.filter((log: any) => {
-                    if (!log.logDate) return false;
-                    return (
-                      new Date(log.logDate).setHours(0, 0, 0, 0) === todayTime
-                    );
-                  }) || [];
-
-                const isLoggedIn =
-                  todayLogs.length > 0 &&
-                  !todayLogs[todayLogs.length - 1]?.logoutTime;
-
-                const todaysSchedule = emp.schedules?.length
-                  ? emp.schedules.map((s: any, i: number) => (
-                      <div key={i} className="flex items-center">
-                        <CalendarDays className="w-3 h-3 text-teal-800" />
-                        <span>
-                          {new Date(s.startTime).toLocaleTimeString("en-GB", {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}{" "}
-                          -{" "}
-                          {new Date(s.endTime).toLocaleTimeString("en-GB", {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </span>
-                      </div>
-                    ))
-                  : "—";
-
-                return (
-                  <div
-                    key={emp.id}
-                    role="button"
-                    tabIndex={0}
-                    className={`cursor-pointer flex p-2 relative rounded-xs ${
-                      isLoggedIn
-                        ? "bg-teal-600"
-                        : "bg-gray-400 border border-gray-300"
-                    }`}
-                  >
-                    {/* LEFT SIDE */}
-                    <div className="flex flex-col gap-1">
-                      {/* NAME + MR / IMAGE */}
-                      <div className="flex items-center gap-2">
-                        {/* You can replace this with an image from /public */}
-                        <img
-                          src="/avater.png"
-                          className="w-10 h-10 rounded-full"
-                        />
-                        {/*   <p className="rounded-full bg-gray-700 w-10 h-10 flex items-center justify-center text-white">
-                          MR
-                        </p> */}
-
-                        <div className="font-bold uppercase text-lg text-gray-200">
-                          {emp.name}
-                        </div>
-                      </div>
-
-                      {/* SCHEDULE + LOGS */}
-                      <div>
-                        <div className="flex flex-col text-sm">
-                          {todaysSchedule}
-
-                          {todayLogs.length === 0 && (
-                            <div className="flex items-center gap-1 text-gray-500 text-xs mt-1">
-                              <ClipboardClock className="w-3 h-3" />
-                              00:00
-                            </div>
-                          )}
-
-                          {todayLogs.map((log: any, i: number) => (
-                            <div key={i} className="flex gap-2 text-xs mt-1">
-                              {log.loginTime && (
-                                <div className="flex items-center text-xs">
-                                  <ClipboardClock className="w-3 h-3" />
-                                  {safeTime(log.loginTime, mounted)}
-                                </div>
-                              )}
-                              {log.logoutTime && (
-                                <div className="flex items-center gap-1">
-                                  {" "}
-                                  - {safeTime(log.logoutTime, mounted)}
-                                </div>
-                              )}
-                            </div>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-
-                    {/* LOGIN / LOGOUT — BOTTOM RIGHT */}
-                    <div className="absolute bottom-2 right-2 text-white">
-                      {isLoggedIn ? (
-                        <button
-                          onClick={() => openAuth(emp, "logout")}
-                          className="cursor-pointer text-green-300"
-                        >
-                          <ClockCheck />
-                        </button>
-                      ) : (
-                        <button
-                          onClick={() => openAuth(emp, "login")}
-                          className="cursor-pointer text-black"
-                        >
-                          <ClockAlert />
-                        </button>
-                      )}
                     </div>
                   </div>
-                );
-              })}
-            </div>
+
+                  {/* LOGIN / LOGOUT — BOTTOM RIGHT */}
+                  <div className="absolute bottom-2 right-2 text-white">
+                    {isLoggedIn ? (
+                      <button
+                        onClick={() => openAuth(emp, "logout")}
+                        className="cursor-pointer text-green-300"
+                      >
+                        <ClockCheck />
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => openAuth(emp, "login")}
+                        className="cursor-pointer text-black"
+                      >
+                        <ClockAlert />
+                      </button>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -333,8 +229,7 @@ export default function CompanyPageClient({ companyData }: any) {
                 authMode === "login" ? "text-teal-600" : "text-red-600"
               }`}
             >
-              {authMode === "login" ? "Login" : "Logout"} –{" "}
-              {selectedEmployee.name}
+              {authMode === "login" ? "Login" : "Logout"} – {selectedEmployee.name}
             </h3>
 
             <input
