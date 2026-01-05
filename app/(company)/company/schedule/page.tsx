aefaa671f9ee35dda5cbe2971e81e8f9a44dc63c
raselrzl <rasel6041@gmail.com>
2026-01-05 17:52:52 +0100

    monthly summery cost

--- app/(company)/company/schedule/page.tsx
+++ app/(company)/company/schedule/page.tsx
@@ -175,7 +175,7 @@ export default function CompanySchedulePage() {
           </div>
 
           {/* Dates */}
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 mt-2">
             <DatePicker
               multiple
               value={selectedDates}
