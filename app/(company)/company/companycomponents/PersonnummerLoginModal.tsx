a893acfef2e2367123c7c5bcba3e57dcb6dc0941
raselrzl <rasel6041@gmail.com>
2026-01-13 20:50:05 +0100

    added design on notification

--- app/(company)/company/companycomponents/PersonnummerLoginModal.tsx
+++ app/(company)/company/companycomponents/PersonnummerLoginModal.tsx
@@ -90,7 +90,7 @@ export default function PersonnummerLoginModal({
         />
 
         {/* KEYPAD */}
-        <div className="grid grid-cols-3 gap-4 w-full max-w-sm">
+        <div className="grid grid-cols-3 gap-4 w-full max-w-sm border">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
             <button
               key={num}
