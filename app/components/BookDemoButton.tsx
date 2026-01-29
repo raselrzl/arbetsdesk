2ce4d9f7336397caf9742aa031d74856d21d481f
raselrzl <rasel6041@gmail.com>
2026-01-29 13:58:57 +0100

    home page done

--- app/components/BookDemoButton.tsx
+++ app/components/BookDemoButton.tsx
@@ -9,11 +9,11 @@ export default function BookDemoButton() {
       initial={{ opacity: 0, scale: 0.9 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.4, ease: "easeOut" }}
-      className="fixed bottom-8 right-10 z-50"
+      className="fixed bottom-6 right-6 z-50"
     >
       <Link
         href="/book-demo"
-        className="bg-teal-900 hover:bg-teal-700 border border-white text-white px-5 py-3 rounded-full text-sm font-semibold shadow shadow-white transition flex items-center gap-2"
+        className="bg-teal-400 hover:bg-teal-700 border border-white text-teal-900 px-3 py-2 rounded-full text-xs font-semibold shadow shadow-white transition flex items-center gap-2"
       >
         Book a Demo
       </Link>
