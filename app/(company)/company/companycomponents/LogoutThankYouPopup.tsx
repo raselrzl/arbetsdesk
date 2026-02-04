53abec34c7358d9e4af6e4398672953dca755921
raselrzl <rasel6041@gmail.com>
2026-02-05 00:20:40 +0100

    update schedule ui

--- app/(company)/company/companycomponents/LogoutThankYouPopup.tsx
+++ app/(company)/company/companycomponents/LogoutThankYouPopup.tsx
@@ -32,11 +32,11 @@ export default function LogoutThankYouPopup({
 
   return (
     <div className="fixed inset-0 z-9999 bg-black/40 flex items-center justify-center">
-      <div className="bg-white w-full max-w-[250px] h-[260px] rounded-xs shadow-xl text-center relative">
-        <div className="text-xl font-bold text-gray-200 bg-[#02505e] py-4 uppercase">
-          Thank you For Today
+      <div className="bg-white w-[260px] h-80 shadow-xl text-center relative flex flex-col justify-between rounded-3xl">
+        <div className="text-xl font-bold text-gray-200 bg-[#02505e] py-4 uppercase rounded-t-2xl">
+          <span className="text-sm text-teal-400">Thank you For Today</span>
           {employeeName && (
-            <p className="text-sm mb-2 font-semibold">
+            <p className="mb-2 font-extrabold text-xl">
               {employeeName}
             </p>
           )}
@@ -47,19 +47,15 @@ export default function LogoutThankYouPopup({
         </p>
 
         <div className="text-sm font-mono mb-5 flex flex-col gap-1">
-          <div className="flex justify-center items-center gap-1">
-            <ClipboardClock className="w-3 h-3" />
-            Login: {safeTime(loginTime)}
-          </div>
-          <div className="flex justify-center items-center gap-1">
-            <ClipboardClock className="w-3 h-3" />
-            Logout: {safeTime(logoutTime)}
+          <div className="flex justify-center items-center gap-1 text-2xl">
+            
+            {safeTime(loginTime)} - {safeTime(logoutTime)}
           </div>
         </div>
 
         <button
           onClick={onClose}
-          className="w-full py-4 bg-[#02505e] text-white font-semibold"
+          className="w-full py-4 bg-[#02505e] text-white font-semibold rounded-b-2xl"
         >
           OK
         </button>
