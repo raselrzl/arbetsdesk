fdbfb4c6b2b65ddc3ded5b5d5e7a9f942d6b5bbc
raselrzl <rasel6041@gmail.com>
2026-02-07 18:21:32 +0100

    seperate vat monthly

--- app/(company)/company/schedule/MonthlySummaryTable.tsx
+++ app/(company)/company/schedule/MonthlySummaryTable.tsx
@@ -91,7 +91,7 @@ export default function MonthlySummaryTable({
   return (
     <div className="mt-12 bg-[#02505e]">
       {/* HEADER */}
-      <div className="flex items-center justify-between px-2">
+      <div className="flex items-center justify-between px-2 border border-teal-100">
         <div className="font-bold text-gray-100 bg-[#02505e] px-4 py-2 uppercase rounded-xs">
           Monthly Summary · {format(start, "yyyy-MM")}
         </div>
